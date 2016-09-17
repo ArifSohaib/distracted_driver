8d82ca7e2b374101c2c946c71008ef157239043a
sohaib <arif_sohaib@outlook.com>
2016-09-17 15:01:18 +0500

    add comments and fix create_distracted script

--- create_distracted.sh
+++ create_distracted.sh
@@ -2,16 +2,21 @@
 # Create the imagenet lmdb inputs
 # N.B. set the path to the imagenet train + val data dirs
 
-EXAMPLE=/home/sohaib/caffe/mydata/distracted_driver
+#Some setup defining the folders.
+#Replace with the path to the folder where you want you lmdb files
+OUTPUT_FOLDER=/home/sohaib/caffe/mydata/distracted_driver_orig
+#Replace with the path to the folder that contains your train folder
 DATA=/home/sohaib/kaggle
+#Replace with the location of your caffe tools directory, which will be in your caffe directory. i.e /path/to/caffe/build/tools
 TOOLS=/home/sohaib/caffe/build/tools
 
-TRAIN_DATA_ROOT=/home/sohaib/kaggle
-VAL_DATA_ROOT=/home/sohaib/kaggle
+#if your validation or training data is somewhere else in the DATA folder, then add the additional path here
+TRAIN_DATA_ROOT=/
+VAL_DATA_ROOT=/
 
 # Set RESIZE=true to resize the images to 256x256. Leave as false if images have
 # already been resized using another tool.
-RESIZE=true
+RESIZE=false
 if $RESIZE; then
   RESIZE_HEIGHT=256
   RESIZE_WIDTH=256
@@ -20,6 +25,7 @@ else
   RESIZE_WIDTH=0
 fi
 
+#Check if data directory exists
 if [ ! -d "$TRAIN_DATA_ROOT" ]; then
   echo "Error: TRAIN_DATA_ROOT is not a path to a directory: $TRAIN_DATA_ROOT"
   echo "Set the TRAIN_DATA_ROOT variable in create_imagenet.sh to the path" \
@@ -33,7 +39,13 @@ if [ ! -d "$VAL_DATA_ROOT" ]; then
        "where the ImageNet validation data is stored."
   exit 1
 fi
+#check if output folder exists
+if [ ! -d "$OUTPUT_FOLDER" ]; then
+  echo "Creating output folder: $OUTPUT_FOLDER"
+  mkdir $OUTPUT_FOLDER
+fi
 
+#actual script to use the convert_imageset tool to make the lmdb files
 echo "Creating train lmdb..."
 
 GLOG_logtostderr=1 $TOOLS/convert_imageset \
@@ -42,7 +54,7 @@ GLOG_logtostderr=1 $TOOLS/convert_imageset \
     --shuffle \
     $TRAIN_DATA_ROOT \
     $DATA/train.txt \
-    $EXAMPLE/train
+    $OUTPUT_FOLDER/train
 
 echo "Creating val lmdb..."
 
@@ -52,6 +64,6 @@ GLOG_logtostderr=1 $TOOLS/convert_imageset \
     --shuffle \
     $VAL_DATA_ROOT \
     $DATA/val.txt \
-    $EXAMPLE/val
+    $OUTPUT_FOLDER/val
 
 echo "Done."
