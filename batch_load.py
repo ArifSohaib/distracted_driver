2ff30ad9926ad2937793472ff1e12777956e1773
sohaib <arif_sohaib@outlook.com>
2016-09-20 11:53:29 +0500

    Annotated batch_load.py

--- batch_load.py
+++ batch_load.py
@@ -1,10 +1,12 @@
-import os;
+'''Utility program to load a batch of image data'''
+
+import os
 import sys
 caffe_root = '../'
 sys.path.insert(0, caffe_root + 'python')
 import gc
 
-import caffe;
+import caffe
 class batch_load:
     def __init__(self, folder,batch_size):
         self.used_files = []
@@ -13,19 +15,27 @@ class batch_load:
         self.batch_size = batch_size
 
     def batch_load(self, verbose = False):
-#        print(folder)
-        image_batch = []
-        batch_file_names = []
+#       print(folder)
+        image_batch = [] #A list of loaded images
+        batch_file_names = [] #A list to keep track of files in a batch
+        #For each file in the image folder
         for image in os.listdir(self.folder):
+            #get the full path of the image file
             IMAGE_FILE = os.path.join(self.folder, image)
+            #add the image to the list of images already used in a batch
             self.used_files.append(IMAGE_FILE)
-            #print(used_files)
+
             if (len(image_batch) < min(self.batch_size, self.total_size)):
+                #When debugging, show which files are being added to a batch
                 if(verbose):
                     print "adding ", IMAGE_FILE
+                #To ensure a file does not get added multiple times, we only add the file if it is not in a batch
                 if(image not in self.used_files):
+                    #load the image using caffe's built-in image loader
                     input_image = caffe.io.load_image(IMAGE_FILE)
+                    #then append the loaded image to the batch
                     image_batch.append(input_image)
+                    #and the file name to the batch
                     batch_file_names.append(image)
                 else:
                     print("file already in batch")
@@ -34,6 +44,7 @@ class batch_load:
                 break
         return batch_file_names, image_batch;
 
+    #Function to test if batch loading works
     def test(self):
         FOLDER = '/home/sohaib/kaggle/test'
         batch = self.batch_load(FOLDER,50)
@@ -47,10 +58,10 @@ class batch_load_testing:
         #list all the files in the folder
         files = os.listdir(self.folder);
         batch = [];
-        #diveide the file names into batches of lists
+        '''diveide the file names into batches of lists eg:
         #batch[0] = files[0:N]
         #batch[1] = files[N+1:N+N]
-        #batch[2] = files[N+N+N+1:N+N+N+N]
+        #batch[2] = files[N+N+N+1:N+N+N+N]'''
         for i in range(0,len(files)/self.batch_size, self.batch_size):
             batch.append(files[i:i+self.batch_size])
             del files[i:i+self.batch_size]
